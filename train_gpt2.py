9dc877d94224c4046287a858418f0c7f09e927fc
Benjamin Chan <ben@vervious.com>
2024-12-03 21:34:12 +0000

    fused optimizer + weight decay

--- train_gpt2.py
+++ train_gpt2.py
@@ -225,7 +225,39 @@ class GPT(nn.Module):
         flops_achieved = flops_per_iter * (1.0/dt)
         return flops_achieved
 
+    def configure_optimizers(self, weight_decay, learning_rate, device):
+        # start with all of the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+
+        # we only weight-decay the weights that participate in matrix multiplications
+        # want to do weight decay for regularization...
+        # i.e. only weight decay all weight tensors in matmuls + embeddings decay; biases and layer norms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+
+        # create AdamW optimizer and use the fused version if it is available
+        # Fused just fuse all the kernels, so a single time, for all the parameters, call a kernel that updates them (morally)
+        import inspect
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        use_fused = fused_available and 'cuda' in device
+        print(f"using fused AdamW: {use_fused}")
+
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused) 
+
+        return optimizer
+
+
 class DataLoaderLite:
+    """Note, we skip the ramp up in batch size from GPT-3, it might only be slightly useful. (Why might ramp up be useful? Because early first order stuff (biases, which tokens should be skipped, etc) don't need large batches to learn (todo aern't datapoints in a batch independent anyways though).)"""
 
     reuseDict = {}
     lastBatchPosition = 0
@@ -320,7 +352,8 @@ def get_lr(it):
     return min_lr + coeff * (max_lr - min_lr)
 
 # Learn
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8) # stealing hyperparams from gpt3
+optimizer = model.configure_optimizers(weight_decay=0.1, learning_rate=6e-4, device=device) # stealing hyperparams from gpt3
+
 for step in range(max_steps):
     t0 = time.time()
     # each datapoint used only once
